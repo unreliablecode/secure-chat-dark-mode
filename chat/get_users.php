eb77f662c5c1564f36d62bca9f5517d8926d02f8
Crystal Oscillator <78525180+unreliablecode@users.noreply.github.com>
2023-10-05 23:20:52 +0700

    Dark Mode!

--- chat/get_users.php
+++ chat/get_users.php
@@ -5,7 +5,7 @@
 
   $query = "SELECT * FROM users WHERE id != $uid ORDER BY username ASC";
   $result = mysqli_query($db, $query);
-  $table = "<table class='table table-hover' id='users-table' style='margin: 40px auto; width:60%; box-shadow: -2px 6px 10px rgb(235, 235, 235); border-bottom: 1px solid #eeeeee'>
+  $table = "<table class='table table-hover' id='users-table' style='margin: 40px auto; width:60%; box-shadow: -2px 6px 10px rgb(130, 129, 129); border-bottom: 1px solid #706f6f'>
               <thead class='table-dark'>
                 <th>User</th>
                 <th>Status</th>
@@ -27,16 +27,16 @@
     }
     else{
       $dotColor = "#6C757D";
-      $chatColor = "#6C757D";
+      $chatColor = "#d95d82";
       $status = "Offline";
     }
 
-    $status = "<span class='receiver-public-statusText'>$status</span>";
+    $status = "<span class='receiver-public-statusText' style='padding-left: 20px; color: white;'>$status</span>";
     $dot = "<i style='font-size:10px; color:$dotColor; transition: all 3s;' class='fas fa-circle align-middle receiver-public-dot'></i>";
 
     $table .= "
     <tr>
-      <td class='fw-light' style='padding-left: 20px;'>".ucfirst($username)."</td>
+    <td style='padding-left: 20px; color: white;'>".ucfirst($username)."</td>
       <td>$status &nbsp $dot</td>
       <td>
         <button class='btn btn-sm border-0' data-bs-toggle='modal' data-bs-target='#chatModal' data-bs-senderId='$uid' data-bs-receiverId='$rid' data-bs-username='$username' data-bs-status='$online'><i style='font-size:18px; color:$chatColor;' class='receiver-public-chatIcon fas fa-comments align-middle'></i></button>

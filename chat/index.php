fc85bf8ab3d7f19852bdc548ad2277f9dfb688de
Crystal Oscillator <78525180+unreliablecode@users.noreply.github.com>
2023-10-05 23:20:23 +0700

    Dark Mode

--- chat/index.php
+++ chat/index.php
@@ -1,6 +1,6 @@
 <?php require('../server/chat_server.php'); ?>
-<!DOCTYPE html>
 
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -9,7 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://kit.fontawesome.com/4733528720.js" crossorigin="anonymous"></script>
 
-  <title>Secured Chat App</title>
+  <title>unreliablecode</title>
   <link rel="stylesheet" type="text/css" href="../resources/css/chat.css">
   <link rel="stylesheet" type="text/css" href="../vendors/css/normalize.css">
   <link rel="stylesheet" type="text/css" href="../vendors/css/Grid.css">
@@ -23,68 +23,65 @@
   <script src='https://cdn.jsdelivr.net/npm/big.js@6.0.0/big.min.js'></script>
 </head>
 
-<body>
-  <h1 class="justify-content-left d-inline">Secured Chat App</h1>
+<body style="background-color: #222; color: #fff; font-family: Arial, sans-serif; display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh; margin: 0;">
+
+  <h1 class="justify-content-left d-inline">unreliablecode</h1>
+
   <?php if(isset($_SESSION['uid']) && isset($_SESSION['username'])): ?>
+
     <div style="float:right;">
       <form href="" method="POST" style="width: 0%; float: right;">
         <button type="submit" style='text-align:left; margin-left:40px; margin-top:3px;' class="btn btn-outline-danger btn-sm" id="logoutBtn" onmouseover="hover('logout');" onmouseout="out('logout');" name="logout">
-            <p class="card-text" id="logoutP"> <strong>Logout</strong><i class="fas fa-sign-out-alt animate__animated animate__fadeInLeft" id="logoutIcon" style="display: none; margin-left:7px;"></i></p>
+          <p class="card-text" id="logoutP"> <strong>Logout</strong><i class="fas fa-sign-out-alt animate__animated animate__fadeInLeft" id="logoutIcon" style="display: none; margin-left:7px;"></i></p>
         </button>
       </form>
     </div>
-  <br/>
+    <br/>
 
-  <h1 style="text-align: center; font-size:48px;" class="animate__animated animate__fadeIn"><?php echo "Welcome, <strong>".$_SESSION['username']; ?></strong>&nbsp<i style="font-size:16px;" class='fas fa-circle align-middle text-success'></i></h1>
-  <?php require_once('get_users.php'); ?>
+    <h1 style="text-align: center; font-size:48px; color=#6696ba;" class="animate__animated animate__fadeIn"><?php echo "Welcome, <strong>".$_SESSION['username']; ?></strong>&nbsp<i style="font-size:16px;" class='fas fa-circle align-middle text-success'></i></h1>
 
-  <div class="modal fade fast" id="chatModal" tabindex="-1" aria-labelledby="chatModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-scrollable">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title fw-light" id="chatModalLabel">Chat with </h5>
-            <div class="animate__animated animate__fadeInDown animate__faster" id="last-seen" style="font-style:italic; font-size:10px; font-weight:300; color:#666; float:right; margin-top:5px; margin-left:5px;">Status</div>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="mb-3">
-              <!-- <label for="chat-box" class="col-form-label">Chat:</label> -->
-              <!-- <input type="text" class="form-control disabled" id="chat-box"> -->
-              <div id="chat-box" style="height:500px; background-color:#efefef; border:1px solid #ddd; overflow: auto; padding:16px; margin: 25px 15px 0px 15px; border-radius:8px;">
-                <!-- <div style="width:100px; height:100px; background-color:red;">SCROLL TEST</div> -->
+    <?php require_once('get_users.php'); ?>
+
+    <div class="modal fade fast" id="chatModal" tabindex="-1" aria-labelledby="chatModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-scrollable">
+        <div class="modal-content" style="background: rgba(255, 255, 255, 0.2); border-radius: 10px; padding: 20px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); backdrop-filter: blur(10px); text-align: center;">
+          <div class="modal-header">
+            <h5 class="modal-title fw-light" id="chatModalLabel">Chat with </h5>
+            <div class="animate__animated animate__fadeInDown animate__faster" id="last-seen" style="font-style:italic; font-size:10px; font-weight:300; color:#6696ba; float:right; margin-top:5px; margin-left:5px;">Status</div>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="mb-3">
+            <div id="chat-box" style="height:500px; background-color:rgba(87,136,173,0.2); border:1px solid #ddd; overflow: auto; padding:16px; margin: 25px 15px 0px 15px; border-radius:8px;"></div>
+          </div>
+          <div class="modal-body">
+            <form href="" method="POST">
+              <div class="mb-3">
+                <textarea class="form-control" id="message-text" oninput="this.style.height = '';this.style.height = this.scrollHeight + 'px'" style="height:60px;" required focus placeholder="Type a message..." onkeyup="messageTextChanged()"></textarea>
+                <select id="enc-method" style="margin-top:15px; padding:8px; cursor:pointer; border-radius:8px; background-color:#8a8a8a; width: 100%;">
+                  <option disabled>Encryption Method - </option>  
+                  <option value="des">DES</option>
+                  <option value="aes">AES</option>
+                  <option selected value="rsa">RSA</option>
+                  <option value="gamal">ElGamal</option>
+                </select>
               </div>
-        </div>
-        <div class="modal-body">
-          <form href="" method="POST">
-            <div class="mb-3">
-              <!-- <label for="message-text" class="col-form-label">Message:</label> -->
-              <textarea class="form-control" id="message-text" oninput="this.style.height = '';this.style.height = this.scrollHeight + 'px'" style="height:60px;" required focus placeholder="Type a message..." onkeyup="messageTextChanged()"></textarea>
-
-              <select id="enc-method" style="margin-top:15px; padding:8px; cursor:pointer; border-radius:8px; background-color:#f3f3f3; width: 100%;">
-                <option disabled>Encryption Method - </option>  
-                <option value="des">DES</option>
-                <option value="aes">AES</option>
-                <option selected value="rsa">RSA</option>
-                <option value="gamal">ElGamal</option>
-              </select>            
-            </div>
-        
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <label class="btn btn-outline-dark" for="upload-file" style="cursor: pointer;"><i style="font-size:16px;" class='fas fa-file-export'></i></label>
-            <input type="file" id="upload-file" name="upload-file" style="opacity: 0; position: absolute; z-index: -1;">
-            <a id="link" download style="display: none"></a>
-          <button type="button" class="btn btn-outline-success" id="send-button" disabled onclick="sendClicked();">Send&nbsp&nbsp<i style="font-size:16px;" class='fas fa-paper-plane'></i></button>
-        </div>
-          </form>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <label class="btn btn-outline-dark" for="upload-file" style="cursor: pointer;"><i style="font-size:16px;" class='fas fa-file-export'></i></label>
+                <input type="file" id="upload-file" name="upload-file" style="opacity: 0; position: absolute; z-index: -1;">
+                <a id="link" download style="display: none"></a>
+                <button type="button" class="btn btn-outline-success" id="send-button" disabled onclick="sendClicked();">Send&nbsp&nbsp<i style="font-size:16px;" class='fas fa-paper-plane'></i></button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
-  </div>
     
   <?php else: ?>
     <div class='text-danger' style='font-size: 16px; font-weight: bold; margin-top:20px; text-align:center;'>Not Logged In, <a href="../" class="text-primary">Login?</a></div>
   <?php endif; ?>
-        
+
 </body>
 </html>
 <?php include_once "../modules/methods.php" ?>
